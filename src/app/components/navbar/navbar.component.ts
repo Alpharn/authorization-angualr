cccc5150b0cfc3721d13f06168ece0713e8228ed
Alpharn <arkrond@gmail.com>
2023-11-13 01:16:23 +0200

    feat: add back button

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 import { AppState } from 'src/app/store/reducers/auth.reducer';
 
@@ -13,19 +13,30 @@ import { AppState } from 'src/app/store/reducers/auth.reducer';
 })
 export class NavbarComponent {
 
-   /** Stream that emits true if the authenticated user is an admin. */
+  /** Stream that emits true if the authenticated user is an admin. */
   isAdmin$: Observable<boolean> = this.store.pipe(
     select(state => localStorage.getItem('role') === 'Admin')
   );
 
+  /** Observable that determines if the back button should be shown based on the current URL. */  
+  showBackButton$: Observable<boolean> = this.router.events.pipe(
+    map(event => this.router.url !== '/dashboard')
+  );
+
   constructor(
     private store: Store<AppState>,
-    private router: Router
+    private router: Router,
   ) {}
-
+   
   /** Logs out the current user by clearing the local storage and navigating to the login page */
   logout(): void {
     localStorage.clear();
     this.router.navigate(['/login']);
   }
+  
+  /** Navigates user to the dashboard page from other pages */
+  goBackToDashboard(): void {
+    this.router.navigate(['/dashboard']);
+  }
+
 }
